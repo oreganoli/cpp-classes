4959fdd1a74ffdd6ebef79a2fe4a0096ee19e438
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-25 18:57:46 +0100

    class 2, exercise 6 - finished

--- Class_02/Ex06/main.cpp
+++ Class_02/Ex06/main.cpp
@@ -22,6 +22,41 @@ int main() {
     string input;
     cout << "Enter an integer:" << endl << ">";
     getline(cin, input);
-    x = stoi(input);
+    try {
+        x = stoi(input);
+    } catch (exception &ex) {
+        cout << ex.what() << endl;
+        cout << "Terminating." << endl;
+        return EXIT_FAILURE;
+    }
+    auto digs = digits(x);
+    int sum = 0;
+    double avg_odd = 0;
+    double counter_odd = 0;
+    double avg_even = 0;
+    double counter_even = 0;
+    for (int dig : *digs) {
+        sum += dig;
+        if (dig % 2 != 0) {
+            avg_odd += dig;
+            counter_odd++;
+        } else {
+            avg_even += dig;
+            counter_even++;
+        }
+    }
+    if (counter_odd != 0) {
+        avg_odd /= counter_odd;
+    }
+    if (counter_even != 0) {
+        avg_even /= counter_even;
+    }
+    cout << "Sum of digits: " << sum << endl;
+    if (counter_odd != 0) {
+        cout << "Ratio of the average of even digits to the average of the odd digits: " << avg_even / avg_odd << endl;
+    } else {
+        cout << "The ratio of the averages of even and odd digits could not be calculated, as there are no odd digits."
+             << endl;
+    }
     return 0;
 }
