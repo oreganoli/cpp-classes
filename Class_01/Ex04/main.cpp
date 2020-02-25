c3496e878fbb47f9014b7067e0d9b7c731b9b993
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-25 16:20:12 +0100

    class 1, exercise 4

---
+++ Class_01/Ex04/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const auto CURRENCY = "PLN";
+const long double TAX_FREE = 556.02;
+const long double BASE_RATE = 0.18;
+const long double UPPER_RATE = 0.32;
+const long double TAX_BRACKET = 85528;
+const long double UPPER_BASE = 14839.02;
+
+auto income_tax(long double income) -> long double {
+    if (income <= TAX_FREE) {
+        return 0;
+    }
+    if (income <= TAX_BRACKET) {
+        return BASE_RATE * income - TAX_FREE;
+    } else {
+        return UPPER_BASE + UPPER_RATE * (income - TAX_BRACKET);
+    }
+}
+
+int main() {
+    string input;
+    cout << "Enter your income [" << CURRENCY <<"]:" << endl << ">";
+    getline(cin, input);
+    auto tax = income_tax(stold(input));
+    cout << "Your income tax is " << tax << " " << CURRENCY << "." << endl;
+    return 0;
+}
