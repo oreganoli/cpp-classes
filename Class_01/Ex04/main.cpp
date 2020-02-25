95256bd3f49f4d0c1bbcc2334352bd68fb7acc19
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-25 16:21:45 +0100

    class 1, exercise 4 - input americanization

--- Class_01/Ex04/main.cpp
+++ Class_01/Ex04/main.cpp
@@ -10,6 +10,14 @@ const long double UPPER_RATE = 0.32;
 const long double TAX_BRACKET = 85528;
 const long double UPPER_BASE = 14839.02;
 
+/// Swaps commas for periods for the purpose of numerical input parsing.
+/// \param s String containing the numerical input to be Americanized
+/// \return void
+auto americanize(std::string *s) {
+    std::replace(s->begin(), s->end(), ',', '.');
+}
+
+
 auto income_tax(long double income) -> long double {
     if (income <= TAX_FREE) {
         return 0;
@@ -25,6 +33,7 @@ int main() {
     string input;
     cout << "Enter your income [" << CURRENCY <<"]:" << endl << ">";
     getline(cin, input);
+    americanize(&input);
     auto tax = income_tax(stold(input));
     cout << "Your income tax is " << tax << " " << CURRENCY << "." << endl;
     return 0;
