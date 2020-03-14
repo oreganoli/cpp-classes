15d93f27e36a811ebc9cc53343599cb4db994aa0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-03-14 13:54:44 +0100

    class 4 (UE), exercise 6

---
+++ Class_04/Ex06/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+// This is the same as UŚ class 4 exercise 3.
+
+auto factorial_iter(unsigned long n) -> unsigned long {
+    if (n == 0) {
+        return 1;
+    }
+    unsigned long output = 1;
+    for (unsigned long i = 1; i <= n; i++) {
+        output *= i;
+    }
+    return output;
+}
+
+auto factorial_recursive(unsigned long n) -> unsigned long {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial_recursive(n - 1);
+}
+
+int main() {
+    printf("factorial_iter(5) = %lu\n", factorial_iter(5));
+    printf("factorial_recursive(5) = %lu\n", factorial_recursive(5));
+    return 0;
+}
