5709314d0c1de2926f84f459ad48f869a51afb3c
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-04-11 16:43:25 +0200

    class 7, exercise 6

--- Class_07/Ex06/main.cpp
+++ Class_07/Ex06/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <sstream>
 #include <optional>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -15,11 +17,49 @@ auto tag(string tag_name, string content, optional<string> properties = nullopt)
     return output.str();
 }
 
+auto split(string input, char delim) -> vector<string> {
+    auto output = vector<string>();
+    int index = 0;
+    int next;
+    do {
+        next = input.find(delim, index);
+        output.push_back(input.substr(index, next - index));
+        index = next + 1;
+    } while (next != -1);
+    return output;
+}
+
+auto htmlize(ifstream &file) -> string {
+    stringstream table_contents;
+    stringstream current_row;
+    bool headers = true;
+    string line;
+    vector<string> elements;
+    while (getline(file, line)) {
+        elements = split(line, ' ');
+        if (headers) {
+            for (int i = 0; i < elements.size(); i++) {
+                current_row << tag("th", elements[i]);
+            }
+        } else {
+            for (int i = 0; i < elements.size(); i++) {
+                current_row << tag("td", elements[i]);
+            }
+        }
+        headers = false;
+        table_contents << tag("tr", current_row.str());
+        current_row.str("");
+    }
+    return tag("table", tag("tbody", table_contents.str()));
+}
+
 int main() {
-    std::cout << tag("html",
-                     tag(
-                             "body",
-                             tag("p", "HTML generation")
-                     )) << std::endl;
+    auto in = ifstream();
+    in.open("../input.txt");
+    string result = htmlize(in);
+    in.close();
+    auto out = ofstream("../output.html");
+    out << result;
+    out.close();
     return 0;
 }
