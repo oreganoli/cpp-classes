8d84855479ac4c4aeed7c20f1e6ae194c44e95b7
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-04-21 20:28:35 +0200

    class 8, exercise 1

--- Class_08/ex01.cpp
+++ Class_08/ex01.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void minMax(int tab[], int w, int* wmin, int* wmax) {}
+void minMax(int tab[], int w, int* wmin, int* wmax) {
+    int min = INT32_MAX;
+    int max = INT32_MIN;
+    for (int i = 0; i < w; i++) {
+        if (tab[i] < min) {
+            min = tab[i];
+        }
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+    *wmin = min;
+    *wmax = max;
+}
 
 int main()
 {
     int t[] = {3, -4, 1, 0, 10, 5};
     int min, max;
     minMax(t, sizeof(t) / sizeof(t[0]), &min, &max);
-    cout << "Najmniejszy i najwieksy element ciągu, to " << min << ", " << max << endl;
+    cout << "Najmniejszy i największy element ciągu to " << min << " i " << max << endl;
     return 0;
 }
